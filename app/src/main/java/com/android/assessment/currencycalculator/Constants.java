4ec514af76bf18697981eaed53a86c1905ac2cd1
Emmanuel Sunesis <kingsonesis1@gmail.com>
2019-08-08 09:49:42 +0100

    Update Constants.java

--- app/src/main/java/com/android/assessment/currencycalculator/Constants.java
+++ app/src/main/java/com/android/assessment/currencycalculator/Constants.java
@@ -8,5 +8,5 @@ public final class Constants {
     /*Please try to obtain your own ACCESS_KEY
     * I have changed it by removing my valid ACCESS_KEY
     * Go to fixer.io to obtain yours*/
-    public static final String ACCESS_KEY = "6b5264b6821697cf09d5ab133b481c2d";
+    public static final String ACCESS_KEY = "xxxxxxxxxxxxxxxxxxx";
 }
